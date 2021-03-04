1e5a3068ba015c8ca62690862fb92b1c5482e996
CarlosJardonIglesias <carlosjardon12@gmail.com>
2021-03-04 23:23:58 +0100

    Hi

--- input.py
+++ input.py
@@ -1,12 +1,18 @@
 name=input("What is your name?")
 print("the name you entered was", name)
-greeting="How old are you, " + name+"?"
-age = input(greeting)
-correction="Has your birthday already happened?"
-birthday=input(correction)
-if birthday=="Yes":
-    year=2021-int(age)
+while True:
+    try:
+        greeting="How old are you, " + name+"?"
+        age = int(input(greeting))
+        break
+    except ValueError:
+        print("Please put your age in numbers...")
+        continue
+correction = "Has your birthday already happened?"
+birthday = input(correction)
+if birthday == "Yes":
+    year = 2021 - int(age)
     print(name, ", you were born in: ", year)
 else:
-    year=2020-int(age)
+    year = 2020 - int(age)
     print(name, ", you were born in: ", year)
